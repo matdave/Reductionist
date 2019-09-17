b7e9ebbe57fd424cd0eb20077b47c2343b88501f
sepiariver <yj+github@paywith.com>
2019-09-17 12:01:35 -0700

    refactor convert test

--- test/cases/ReductionistTest.php
+++ test/cases/ReductionistTest.php
@@ -15,7 +15,7 @@ class ReductionistTest extends TestCase
     protected $quality = 60;
     protected $formats = ['jpg' => IMAGETYPE_JPEG, 'png' => IMAGETYPE_PNG, 'gif' => IMAGETYPE_GIF];
     protected $newFormats = ['webp' => IMAGETYPE_WEBP, 'jp2' => IMAGETYPE_JP2];
-    protected $testNewFormats = false;
+    protected $testNewFormats = true;
     protected $graphicsLib = 2;
     
     protected function setUp(): void
@@ -24,11 +24,14 @@ class ReductionistTest extends TestCase
         require_once(dirname($this->basePath) . '/vendor/autoload.php');
         require_once(dirname($this->basePath) . '/src/Reductionist/Reductionist.php');
         $this->reductionist = new Reductionist($this->graphicsLib);
+        if (!is_dir($this->basePath . $this->outDir)) mkdir($this->basePath . $this->outDir);
     }
+
     public function testConfig()
     {
         $this->assertTrue($this->reductionist instanceof Reductionist);
     }
+
     public function testResizeWidth() 
     {
         $dir = $this->basePath . $this->srcDir;
@@ -50,6 +53,7 @@ class ReductionistTest extends TestCase
             if ($this->removeOutput) unlink($thumb);
         }
     }
+
     public function testResizeHeight() 
     {
         $dir = $this->basePath . $this->srcDir;
@@ -71,11 +75,13 @@ class ReductionistTest extends TestCase
             if ($this->removeOutput) unlink($thumb);
         }
     }
+
     public function testConvertFormat()
     {
         $this->removeOutput = false;
         $dir = $this->basePath . $this->srcDir;
         $files = scandir($dir);
+        $failures = [];
         foreach ($files as $file) {
             if (strpos($file, '.') === 0) continue;
             if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
@@ -89,13 +95,20 @@ class ReductionistTest extends TestCase
                     $thumb,
                     ['h' => $this->height, 'q' => $this->quality]
                 );
-                $this->assertFileExists($thumb, 'Failed: ' . $thumb);
-                $this->assertFileNotEquals($full, $thumb,  'Failed: ' . $thumb);
-                $this->assertGreaterThan(filesize($thumb), filesize($full), 'Failed: ' . $thumb);
-                $this->assertEquals(getimagesize($thumb)[1], $this->height, 'Failed: ' . $thumb);
-                $this->assertEquals($type, getimagesize($thumb)[2]);
+                try {
+                    $this->assertFileExists($thumb);
+                    $this->assertFileNotEquals($full, $thumb);
+                    $this->assertGreaterThan(filesize($thumb), filesize($full));
+                    $this->assertEquals(getimagesize($thumb)[1], $this->height);
+                    $this->assertEquals($type, getimagesize($thumb)[2]);
+                } catch(PHPUnit_Framework_ExpectationFailedException $e) {
+                    $failures[] = $e->getMessage();
+                }
                 if ($this->removeOutput) unlink($thumb);
             }
+        }
+        if(!empty($failures)) {
+            throw new PHPUnit_Framework_ExpectationFailedException (count($failures) . " assertions failed:\n\t" . implode("\n\t", $failures));
         }
     }
 }
