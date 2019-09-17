4f9d53397e6b60597b04df63272a44f60d0bb44e
sepiariver <yj+github@paywith.com>
2019-09-17 14:01:29 -0700

    bunch more tests except wmt no workey

--- test/cases/ReductionistTest.php
+++ test/cases/ReductionistTest.php
@@ -9,12 +9,13 @@ class ReductionistTest extends TestCase
     protected $basePath;
     protected $srcDir = '/assets/';
     protected $outDir = '/output/';
-    protected $removeOutput = true;
-    protected $width = 400;
-    protected $height = 300;
+    protected $removeOutput = false;
+    protected $width = 450;
+    protected $height = 250;
     protected $quality = 60;
     protected $formats = ['jpg' => IMAGETYPE_JPEG, 'png' => IMAGETYPE_PNG, 'gif' => IMAGETYPE_GIF];
-    protected $newFormats = ['webp' => IMAGETYPE_WEBP, 'jp2' => IMAGETYPE_JP2];
+    protected $newFormats = ['webp' => IMAGETYPE_WEBP];
+    protected $todoFormats = ['jp2' => IMAGETYPE_JP2];
     protected $testNewFormats = true;
     protected $graphicsLib = 2;
     
@@ -24,6 +25,7 @@ class ReductionistTest extends TestCase
         require_once(dirname($this->basePath) . '/vendor/autoload.php');
         require_once(dirname($this->basePath) . '/src/Reductionist/Reductionist.php');
         $this->reductionist = new Reductionist($this->graphicsLib);
+        $this->reductionist->debug = true;
         if (!is_dir($this->basePath . $this->outDir)) mkdir($this->basePath . $this->outDir);
     }
 
@@ -40,7 +42,7 @@ class ReductionistTest extends TestCase
             if (strpos($file, '.') === 0) continue;
             if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
             $full = $this->basePath . $this->srcDir . $file;
-            $thumb = $this->basePath . $this->outDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'width-' . $file;
             $this->reductionist->processImage(
                 $full,
                 $thumb,
@@ -62,7 +64,7 @@ class ReductionistTest extends TestCase
             if (strpos($file, '.') === 0) continue;
             if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
             $full = $this->basePath . $this->srcDir . $file;
-            $thumb = $this->basePath . $this->outDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'height-' . $file;
             $this->reductionist->processImage(
                 $full,
                 $thumb,
@@ -72,6 +74,128 @@ class ReductionistTest extends TestCase
             $this->assertFileNotEquals($full, $thumb);
             $this->assertGreaterThan(filesize($thumb), filesize($full));
             $this->assertEquals(getimagesize($thumb)[1], $this->height);
+            if ($this->removeOutput) unlink($thumb);
+        }
+    }
+
+    public function testResizeFar() 
+    {
+        $dir = $this->basePath . $this->srcDir;
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (strpos($file, '.') === 0) continue;
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
+            $full = $this->basePath . $this->srcDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'far-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['w' => $this->width, 'h' => $this->height, 'q' => $this->quality, 'far' => 'c', 'bg' => '000']
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
+            $this->assertGreaterThan(filesize($thumb), filesize($full));
+            $this->assertEquals(getimagesize($thumb)[0], $this->width);
+            $this->assertEquals(getimagesize($thumb)[1], $this->height);
+            if ($this->removeOutput) unlink($thumb);
+        }
+    }
+
+    public function testResizeZc() 
+    {
+        $dir = $this->basePath . $this->srcDir;
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (strpos($file, '.') === 0) continue;
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
+            $full = $this->basePath . $this->srcDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'zc-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['w' => $this->width, 'h' => $this->height, 'q' => $this->quality, 'zc' => 'c']
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
+            $this->assertGreaterThan(filesize($thumb), filesize($full));
+            $this->assertEquals(getimagesize($thumb)[0], $this->width);
+            $this->assertEquals(getimagesize($thumb)[1], $this->height);
+            if ($this->removeOutput) unlink($thumb);
+        }
+    }
+
+    public function testResizeScale() 
+    {
+        $dir = $this->basePath . $this->srcDir;
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (strpos($file, '.') === 0) continue;
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
+            $full = $this->basePath . $this->srcDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'scalew-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['w' => $this->width, 'q' => $this->quality, 'scale' => 2]
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
+            $this->assertGreaterThan(filesize($thumb), filesize($full));
+            $this->assertEquals(getimagesize($thumb)[0], (int) $this->width * 2);
+            if ($this->removeOutput) unlink($thumb);
+
+            $thumb = $this->basePath . $this->outDir . 'scaleh-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['h' => $this->height, 'q' => $this->quality, 'scale' => 2]
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
+            $this->assertGreaterThan(filesize($thumb), filesize($full));
+            $this->assertEquals(getimagesize($thumb)[1], (int) $this->height * 2);
+            if ($this->removeOutput) unlink($thumb);
+        }
+    }
+
+    public function testWmt() 
+    {
+        $dir = $this->basePath . $this->srcDir;
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (strpos($file, '.') === 0) continue;
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
+            $full = $this->basePath . $this->srcDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'wmt-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['w' => $this->width, 'fltr' => ['wmt|TEST|50']]
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
+            if ($this->removeOutput) unlink($thumb);
+        }
+        $this->assertTrue(false, implode(PHP_EOL, $this->reductionist->debugmessages));
+    }
+
+    public function testWmi() 
+    {
+        $dir = $this->basePath . $this->srcDir;
+        $wm = $this->basePath . '/cases/sepia.png';
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (strpos($file, '.') === 0) continue;
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) continue;
+            $full = $this->basePath . $this->srcDir . $file;
+            $thumb = $this->basePath . $this->outDir . 'wmi-' . $file;
+            $this->reductionist->processImage(
+                $full,
+                $thumb,
+                ['w' => $this->width, 'fltr' => ['wmi|' . $wm . '|C|50']]
+            );
+            $this->assertFileExists($thumb);
+            $this->assertFileNotEquals($full, $thumb);
             if ($this->removeOutput) unlink($thumb);
         }
     }
@@ -93,13 +217,12 @@ class ReductionistTest extends TestCase
                 $this->reductionist->processImage(
                     $full,
                     $thumb,
-                    ['h' => $this->height, 'q' => $this->quality]
+                    ['q' => $this->quality]
                 );
                 try {
                     $this->assertFileExists($thumb);
                     $this->assertFileNotEquals($full, $thumb);
-                    $this->assertGreaterThan(filesize($thumb), filesize($full));
-                    $this->assertEquals(getimagesize($thumb)[1], $this->height);
+                    $this->assertNotEquals($type, getimagesize($full)[2]);
                     $this->assertEquals($type, getimagesize($thumb)[2]);
                 } catch(PHPUnit_Framework_ExpectationFailedException $e) {
                     $failures[] = $e->getMessage();
